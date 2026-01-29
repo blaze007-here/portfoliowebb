97b5d48f4defa0a162456f9d223d62182a213b3d
Vyom Goyal <vyomgoyal07@example.com>
2026-01-29 21:37:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll(".btn").forEach(btn => {
+  btn.addEventListener("mousedown", () => {
+    btn.style.transform = "scale(0.94)";
+  });
+  btn.addEventListener("mouseup", () => {
+    btn.style.transform = "scale(1)";
+  });
+});
